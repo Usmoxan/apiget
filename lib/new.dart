9d4bdb761e357d0d4c37f175f50fc141553348de
Usmoxan <usmoxan@gmail.com>
2022-11-28 00:52:59 +0500

    first commit

---
+++ lib/new.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Okloo extends StatefulWidget {
+  const Okloo({super.key});
+
+  @override
+  State<Okloo> createState() => _OklooState();
+}
+
+class _OklooState extends State<Okloo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
